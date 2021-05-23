783204478894124751b42f8d9d0513a9472b9372
TDLorenz <troydaniello.nyc@gmail.com>
2021-05-22 23:58:52 -0400

    added put method for campuses in routes/campus.js

--- routes/campuses.js
+++ routes/campuses.js
@@ -6,24 +6,24 @@ const ash = require('express-async-handler');
 
 /** GET ALL CAMPUSES */
 
-router.get('/', ash(async(req, res) => {
-  let campuses = await Campus.findAll({include: [Student]});
+router.get('/', ash(async (req, res) => {
+  let campuses = await Campus.findAll({ include: [Student] });
   res.status(200).json(campuses);
 }));
 
 /** GET CAMPUS BY ID*/
-router.get('/:id', ash(async(req, res) => {
-  let campus = await Campus.findByPk(req.params.id, {include: [Student]});
+router.get('/:id', ash(async (req, res) => {
+  let campus = await Campus.findByPk(req.params.id, { include: [Student] });
   res.status(200).json(campus);
 }));
 
-router.post('/', function(req, res, next) {
+router.post('/', function (req, res, next) {
   Campus.create(req.body)
     .then(createdCampus => res.status(200).json(createdCampus))
     .catch(err => next(err));
 });
 
-router.delete('/:id', function(req, res, next) {
+router.delete('/:id', function (req, res, next) {
   Campus.destroy({
     where: {
       id: req.params.id
@@ -32,6 +32,15 @@ router.delete('/:id', function(req, res, next) {
     .then(() => res.status(200).json("Deleted a campus!"))
     .catch(err => next(err));
 });
+
+/******************* EDIT *********************/
+router.put('/:id', ash(async (req, res) => {
+  await Student.update(req.body,
+    { where: { id: req.params.id } }
+  );
+  let campus = await Campus.findByPk(req.params.id);
+  res.status(201).json(campus);
+}));
 
 // Export our router, so that it can be imported to construct our apiRouter;
 module.exports = router;
